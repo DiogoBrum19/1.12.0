a5f9867fd727c000bf20318c6b625cbeabfe2a14
DiogoBrum19 <130003743+DiogoBrum19@users.noreply.github.com>
2024-01-19 21:59:35 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Message from './message'
+import Titulo from './titulo'
+import axios from 'axios'
+
+function App() {
+  const [loading, setLoading] = React.useState(true);
+  const [mensagens, setMensagens] = React.useState([]);
+  const [erro, setErro] = React.useState(false);
+
+  async function requisao() {
+    try {
+      const resultado = await axios.get('http://localhost:8080/');
+      setMensagens(resultado.data);
+      setLoading(false);
+      setErro(false);
+
+    } catch {
+      setErro(true);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    requisao();
+  }, []);
+
+  if (erro) {
+    return (
+      <div>Erro</div>
+    )
+  }
+  return (
+    <>
+      <body>
+        <div class="chat">
+          <Titulo />
+          <div class="content">
+            {
+              loading === false && (
+                <ul>
+                  {
+                    mensagens.map(mensagem => (
+                      <Message
+                        mensagem={mensagem.mensagem}
+                        visualizado={mensagem.visualizado}
+                        remetente={mensagem.remetente} />
+                    ))
+                  }
+
+                </ul>
+              )
+            }
+            {
+              loading === true && (
+                <div>
+                  <div className='skeleton-loader' />
+                  <div className='skeleton-loader' />
+                  <div className='skeleton-loader' />
+                </div>
+              )
+
+            }
+          </div>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App;
---
+++ src/titulo.jsx
@@ -0,0 +1,10 @@
+import './titulo.css'
+import Button from './button'
+export default function Titulo() {
+    return (
+      <div className='header'>
+        <h3>Meu Chat</h3>
+        <Button></Button>
+      </div>
+    )
+  }
